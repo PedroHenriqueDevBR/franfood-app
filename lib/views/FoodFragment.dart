c34b86253c0f411609b002fd1f5ae5d4ea4fc6c5
PedroHenriqueDevBR <p.lyma18@gmail.com>
2020-04-19 16:09:46 -0300

    style: show card food

---
+++ lib/views/FoodFragment.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class FoodFragment extends StatefulWidget {
+  @override
+  _FoodFragmentState createState() => _FoodFragmentState();
+}
+
+class _FoodFragmentState extends State<FoodFragment> {
+  List<CardFood> items = [
+    CardFood(
+      'Bomba',
+      'Bomba de presunto e queijo',
+      'https://images.pexels.com/photos/41967/appetizer-canape-canapes-cheese-41967.jpeg',
+    ),
+    CardFood(
+      'Bomba',
+      'Bomba de presunto e queijo',
+      'https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    ),
+    CardFood(
+      'Bomba',
+      'Bomba de presunto e queijo',
+      'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    ),
+    CardFood(
+      'Bomba',
+      'Bomba de presunto e queijo',
+      'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    ),
+  ];
+
+  Widget _cardFood(String title, String subtitle, String image) {
+    return Container(
+      height: 250.0,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w700,
+              shadows: [
+                Shadow(
+                  color: Colors.black,
+                  blurRadius: 15,
+                ),
+              ],
+            ),
+          ),
+          Text(
+            subtitle,
+            style: TextStyle(
+              fontSize: 20.0,
+              shadows: [
+                Shadow(color: Colors.black, blurRadius: 15),
+              ],
+            ),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16.0),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(image),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: items.length,
+      separatorBuilder: (context, index) => Divider(
+        color: Colors.transparent,
+      ),
+      itemBuilder: (context, index) => _cardFood(
+        items[index].title,
+        items[index].subtitle,
+        items[index].image,
+      ),
+    );
+  }
+}
+
+class CardFood {
+  String title;
+  String subtitle;
+  String image;
+
+  CardFood(this.title, this.subtitle, this.image);
+}
--- lib/views/IndexActivity.dart
+++ lib/views/IndexActivity.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:share/share.dart';
+import '../views/FoodFragment.dart';
 
 class IndexActivity extends StatefulWidget {
   @override
@@ -9,48 +10,86 @@ class IndexActivity extends StatefulWidget {
 
 class _IndexActivityState extends State<IndexActivity> {
   final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+
   String url = 'https://franfood.herokuapp.com/';
+  int _currentIndex = 0;
+  List<Widget> _children = [FoodFragment(), FoodFragment(), FoodFragment()];
+
+  void _changeIndex(index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+      body: _children[_currentIndex],
+      bottomNavigationBar: bottomNavigationBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text("Fran Food"),
+      leading: Icon(Icons.fastfood),
+      backgroundColor: Colors.red[900],
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.share),
+          onPressed: () {
+            final RenderBox box = context.findRenderObject();
+            Share.share(url,
+                subject: url,
+                sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+          },
+        ),
+      ],
+    );
+  }
+
+  BottomNavigationBar bottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      onTap: _changeIndex,
+      backgroundColor: Colors.red[900],
+      selectedItemColor: Colors.white,
+      elevation: 8.0,
+      showUnselectedLabels: false,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.room_service),
+          title: Text('Lanches'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.local_drink),
+          title: Text('Bebidas'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.restaurant),
+          title: Text('Combos'),
+        ),
+      ],
+    );
+  }
+
+  // Página de apresentação funcional
+  WebviewScaffold homeWebView() {
     return WebviewScaffold(
-      key: _globalKey,
-      debuggingEnabled: false,
-      withJavascript: true,
-      scrollBar: false,
-      appCacheEnabled: true,
-      withOverviewMode: true,
-      ignoreSSLErrors: true,
-      withLocalStorage: true,
-      useWideViewPort: true,
-      url: url,
-      initialChild: Center(child: CircularProgressIndicator(),),
-      appBar: AppBar(
-        title: Text("Fran Food App"),
-        backgroundColor: Colors.black,
-        leading: Icon(Icons.fastfood),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.share),
-            onPressed: () {
-              final RenderBox box = context.findRenderObject();
-              Share.share(url,
-                  subject: url,
-                  sharePositionOrigin:
-                  box.localToGlobal(Offset.zero) &
-                  box.size);
-            },
-          ),
-        ],
-      ),
-    );
+        key: _globalKey,
+        debuggingEnabled: false,
+        withJavascript: true,
+        scrollBar: false,
+        appCacheEnabled: true,
+        withOverviewMode: true,
+        ignoreSSLErrors: true,
+        withLocalStorage: true,
+        useWideViewPort: true,
+        url: url,
+        initialChild: Center(
+          child: CircularProgressIndicator(),
+        ),
+        appBar: appBar());
   }
-
-  Container _contactBottomSheet(ctx) {
-    return Container(
-      child: Text('Container Bottom sheet'),
-    );
-  }
-
 }
-
